1d98a6e8a9a593290cb77983012fb16a45887e12
Bruno Meilick <b@bnomei.com>
2023-03-11 10:32:56 +0000

    :sparkles: added icon to UTM dashboard blueprint
    Signed-off-by: bnomei <b@bnomei.com>

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ panel.plugin('bnomei/utm', {
   fields: {
     'utmbar': Utmbar,
   },
+  icons: {
+    "utm-bars": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><g fill="currentColor" class="nc-icon-wrapper"><rect width="6" height="30" x="12" y="14" data-color="color-2" rx="1" ry="1"/><rect width="6" height="20" x="21" y="24" rx="1" ry="1"/><rect width="6" height="30" x="30" y="14" data-color="color-2" rx="1" ry="1"/><rect width="6" height="20" x="39" y="24" rx="1" ry="1"/><rect width="6" height="40" x="3" y="4" rx="1" ry="1"/></g></svg>',
+  },
 });
